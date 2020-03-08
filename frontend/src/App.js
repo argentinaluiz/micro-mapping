bb2ac5f8b9550db9985232de6e56459597c8171d
Luiz <argentinaluiz@gmail.com>
2020-03-08 00:58:50 -0300

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter} from "react-router-dom";
+import {Switch, Route} from "react-router-dom";
+import Mapping from "./pages/Mapping";
+import {SnackbarProvider} from "notistack";
+
+function App() {
+    return (
+        <SnackbarProvider>
+            <BrowserRouter>
+                <Switch>
+                    <Route path={'/orders/:id/mapping'} component={Mapping} exact={true}/>
+                </Switch>
+            </BrowserRouter>
+        </SnackbarProvider>
+    );
+}
+
+export default App;
